4170c31d6756a1fa1b8b7c53533a57e665f548e5
Timothee Chauvin <timothee.chauvin28@gmail.com>
2025-05-17 15:47:12 +0200

    install pre-commit

--- construct_email.py
+++ construct_email.py
@@ -1,12 +1,14 @@
-from paper import ArxivPaper
+import datetime
 import math
-from tqdm import tqdm
+import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
-from email.utils import parseaddr, formataddr
-import smtplib
-import datetime
+from email.utils import formataddr, parseaddr
+
 from loguru import logger
+from tqdm import tqdm
+
+from paper import ArxivPaper
 
 framework = """
 <!DOCTYPE HTML>
@@ -46,8 +48,9 @@ To unsubscribe, remove your email in your Github Action setting.
 </html>
 """
 
+
 def get_empty_html():
-  block_template = """
+    block_template = """
   <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; border: 1px solid #ddd; border-radius: 8px; padding: 16px; background-color: #f9f9f9;">
   <tr>
     <td style="font-size: 20px; font-weight: bold; color: #333;">
@@ -56,10 +59,24 @@ def get_empty_html():
   </tr>
   </table>
   """
-  return block_template
+    return block_template
 
-def get_block_html(title:str, authors:str, rate:str,arxiv_id:str, abstract:str, pdf_url:str, code_url:str=None, affiliations:str=None):
-    code = f'<a href="{code_url}" style="display: inline-block; text-decoration: none; font-size: 14px; font-weight: bold; color: #fff; background-color: #5bc0de; padding: 8px 16px; border-radius: 4px; margin-left: 8px;">Code</a>' if code_url else ''
+
+def get_block_html(
+    title: str,
+    authors: str,
+    rate: str,
+    arxiv_id: str,
+    abstract: str,
+    pdf_url: str,
+    code_url: str = None,
+    affiliations: str = None,
+):
+    code = (
+        f'<a href="{code_url}" style="display: inline-block; text-decoration: none; font-size: 14px; font-weight: bold; color: #fff; background-color: #5bc0de; padding: 8px 16px; border-radius: 4px; margin-left: 8px;">Code</a>'
+        if code_url
+        else ""
+    )
     block_template = """
     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="font-family: Arial, sans-serif; border: 1px solid #ddd; border-radius: 8px; padding: 16px; background-color: #f9f9f9;">
     <tr>
@@ -98,65 +115,83 @@ def get_block_html(title:str, authors:str, rate:str,arxiv_id:str, abstract:str, 
     </tr>
 </table>
 """
-    return block_template.format(title=title, authors=authors,rate=rate,arxiv_id=arxiv_id, abstract=abstract, pdf_url=pdf_url, code=code, affiliations=affiliations)
+    return block_template.format(
+        title=title,
+        authors=authors,
+        rate=rate,
+        arxiv_id=arxiv_id,
+        abstract=abstract,
+        pdf_url=pdf_url,
+        code=code,
+        affiliations=affiliations,
+    )
 
-def get_stars(score:float):
+
+def get_stars(score: float):
     full_star = '<span class="full-star">⭐</span>'
     half_star = '<span class="half-star">⭐</span>'
     low = 6
     high = 8
     if score <= low:
-        return ''
+        return ""
     elif score >= high:
         return full_star * 5
     else:
-        interval = (high-low) / 10
-        star_num = math.ceil((score-low) / interval)
-        full_star_num = int(star_num/2)
+        interval = (high - low) / 10
+        star_num = math.ceil((score - low) / interval)
+        full_star_num = int(star_num / 2)
         half_star_num = star_num - full_star_num * 2
-        return '<div class="star-wrapper">'+full_star * full_star_num + half_star * half_star_num + '</div>'
+        return '<div class="star-wrapper">' + full_star * full_star_num + half_star * half_star_num + "</div>"
 
 
-def render_email(papers:list[ArxivPaper]):
+def render_email(papers: list[ArxivPaper]):
     parts = []
-    if len(papers) == 0 :
-        return framework.replace('__CONTENT__', get_empty_html())
-    
-    for p in tqdm(papers,desc='Rendering Email'):
+    if len(papers) == 0:
+        return framework.replace("__CONTENT__", get_empty_html())
+
+    for p in tqdm(papers, desc="Rendering Email"):
         rate = get_stars(p.score)
         authors = [a.name for a in p.authors[:5]]
-        authors = ', '.join(authors)
+        authors = ", ".join(authors)
         if len(p.authors) > 5:
-            authors += ', ...'
+            authors += ", ..."
         if p.affiliations is not None:
             affiliations = p.affiliations[:5]
-            affiliations = ', '.join(affiliations)
+            affiliations = ", ".join(affiliations)
             if len(p.affiliations) > 5:
-                affiliations += ', ...'
+                affiliations += ", ..."
         else:
-            affiliations = 'Unknown Affiliation'
-        parts.append(get_block_html(p.title, authors,rate,p.arxiv_id ,p.tldr, p.pdf_url, p.code_url, affiliations))
+            affiliations = "Unknown Affiliation"
+        parts.append(get_block_html(p.title, authors, rate, p.arxiv_id, p.tldr, p.pdf_url, p.code_url, affiliations))
 
-    content = '<br>' + '</br><br>'.join(parts) + '</br>'
-    return framework.replace('__CONTENT__', content)
+    content = "<br>" + "</br><br>".join(parts) + "</br>"
+    return framework.replace("__CONTENT__", content)
 
-def send_email(sender:str, receiver:str, password:str,smtp_server:str,smtp_port:int, html:str,):
+
+def send_email(
+    sender: str,
+    receiver: str,
+    password: str,
+    smtp_server: str,
+    smtp_port: int,
+    html: str,
+):
     def _format_addr(s):
         name, addr = parseaddr(s)
-        return formataddr((Header(name, 'utf-8').encode(), addr))
+        return formataddr((Header(name, "utf-8").encode(), addr))
 
-    msg = MIMEText(html, 'html', 'utf-8')
-    msg['From'] = _format_addr('Github Action <%s>' % sender)
-    msg['To'] = _format_addr('You <%s>' % receiver)
-    today = datetime.datetime.now().strftime('%Y/%m/%d')
-    msg['Subject'] = Header(f'Daily arXiv {today}', 'utf-8').encode()
+    msg = MIMEText(html, "html", "utf-8")
+    msg["From"] = _format_addr(f"Github Action <{sender}>")
+    msg["To"] = _format_addr(f"You <{receiver}>")
+    today = datetime.datetime.now().strftime("%Y/%m/%d")
+    msg["Subject"] = Header(f"Daily arXiv {today}", "utf-8").encode()
 
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
     except Exception as e:
         logger.warning(f"Failed to use TLS. {e}")
-        logger.warning(f"Try to use SSL.")
+        logger.warning("Try to use SSL.")
         server = smtplib.SMTP_SSL(smtp_server, smtp_port)
 
     server.login(sender, password)
--- llm.py
+++ llm.py
@@ -1,12 +1,14 @@
-from llama_cpp import Llama
-from openai import OpenAI
-from loguru import logger
 from time import sleep
+
+from llama_cpp import Llama
+from loguru import logger
+from openai import OpenAI
 
 GLOBAL_LLM = None
 
+
 class LLM:
-    def __init__(self, api_key: str = None, base_url: str = None, model: str = None,lang: str = "English"):
+    def __init__(self, api_key: str = None, base_url: str = None, model: str = None, lang: str = "English"):
         if api_key:
             self.llm = OpenAI(api_key=api_key, base_url=base_url)
         else:
@@ -34,12 +36,14 @@ class LLM:
                     sleep(3)
             return response.choices[0].message.content
         else:
-            response = self.llm.create_chat_completion(messages=messages,temperature=0)
+            response = self.llm.create_chat_completion(messages=messages, temperature=0)
             return response["choices"][0]["message"]["content"]
+
 
 def set_global_llm(api_key: str = None, base_url: str = None, model: str = None, lang: str = "English"):
     global GLOBAL_LLM
     GLOBAL_LLM = LLM(api_key=api_key, base_url=base_url, model=model, lang=lang)
+
 
 def get_llm() -> LLM:
     if GLOBAL_LLM is None:
